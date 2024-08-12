dfc75dd8c18108f80ccad3e7561bc9945f5c1738
Shep0rt <96770277+Shep0rt@users.noreply.github.com>
2024-08-12 09:44:26 +0500

    Update data.py

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ order_body = {
     "metroStation": 1,
     "phone": "+12345678901",
     "rentTime": 5,
-    "deliveryDate": "2024-09-009",
+    "deliveryDate": "2024-09-09",
     "comment": "Хочу лучший самокат",
     "color": [
         "BLACK"
